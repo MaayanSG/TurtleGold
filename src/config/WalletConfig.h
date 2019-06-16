16121feb784bdd9a516f86270a8084ecb80ca955
Maayan <jjksvlogs@gmail.com>
2019-06-16 10:09:38 -0400

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -37,7 +37,7 @@ namespace WalletConfig
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
 
     /* Where can your users contact you for support? E.g. discord */
-    const std::string contactLink = "https://discord.gg/YK38jHp";
+    const std::string contactLink = "https://discord.gg/3XXHybD";
 
     /* The number of decimals your coin has */
     const uint8_t numDecimalPlaces = CryptoNote::parameters
