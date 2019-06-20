a41155e46e78f076acb3544a9cb078686d0d7d9e
Richard <rioea1@outlook.com>
2019-06-20 16:11:11 +0200

    Added more Mnemonics + Small wallet message changes

--- src/Mnemonics/WordList.h
+++ src/Mnemonics/WordList.h
@@ -1,7 +1,8 @@
 //
 // Copyright 2014-2018 The Monero Developers
 // Copyright 2018 The TurtleCoin Developers
-//
+// Copyright 2019 The TurtleGold Developers
+
 // Please see the included LICENSE file for more information.
 
 namespace Mnemonics
