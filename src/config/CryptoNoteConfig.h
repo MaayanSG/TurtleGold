dd8b789993eee785f3fb6b14a628b5d01f6fc54b
Richard <37025447+MysticCraftYT@users.noreply.github.com>
2019-08-23 17:21:22 +0200

    Fix latest version url
    Maayan, FFS READ "const char     LATEST_VERSION_URL[]" latest version should be there, stooped, remember to update it for each release.

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -213,7 +213,7 @@ const uint64_t DATABASE_READ_BUFFER_MB_DEFAULT_SIZE          = 10;
 const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES               = 100;
 const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT     = 2;
 
-const char     LATEST_VERSION_URL[]                          = "https://github.com/turtlegold/TurtleGold/releases";
+const char     LATEST_VERSION_URL[]                          = "https://github.com/turtlegold/TurtleGold/releases/tag/v6.1.0";
 const std::string LICENSE_URL                                = "https://github.com/turtlegold/TurtleGold/blob/master/LICENSE";
 const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 {
